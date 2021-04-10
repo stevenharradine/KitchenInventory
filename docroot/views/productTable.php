1d2c786ee54108860fb0899e2a20f43f896100d8
Steven Harradine <stevenharradine@gmail.com>
2021-04-10 02:55:24 -0400

    autodetect UPC and EAN formats when applicable

--- docroot/views/productTable.php
+++ docroot/views/productTable.php
@@ -11,8 +11,20 @@
       <tbody>
 <?php
     while($row = $result->fetch_assoc()) {
+      $format_fragment = "";
+      if (strlen ($row[barcode]) == 13) {
+        $format_fragment = 'jsbarcode-format="EAN13"';
+      } else if (strlen ($row[barcode]) == 12) {
+        $format_fragment = 'jsbarcode-format="UPC"';
+      } else if (strlen ($row[barcode]) == 8) {
+        $format_fragment = 'jsbarcode-format="EAN8"';
+      } else if (strlen ($row[barcode]) == 5) {
+        $format_fragment = 'jsbarcode-format="EAN5"';
+      } else if (strlen ($row[barcode]) == 2) {
+        $format_fragment = 'jsbarcode-format="EAN2"';
+      }
       echo "<tr>";
-      echo "<td><svg class='barcode' jsbarcode-value='$row[barcode]' jsbarcode-textmargin='0' jsbarcode-fontoptions='bold'></svg></td>";
+      echo "<td><svg class='barcode' $format_fragment jsbarcode-value='$row[barcode]' jsbarcode-textmargin='0' jsbarcode-fontoptions='bold'></svg></td>";
       echo "<td><a href='viewProduct.php?barcode=$row[barcode]'>$row[name]</a></td>";
       echo "<td>$row[size]</td>";
       echo "<td>$row[unit]</td>";
