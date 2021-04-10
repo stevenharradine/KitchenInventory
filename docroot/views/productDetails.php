34cea402dd68ef65e1a5862db8aaafb0198c6c6b
Steven Harradine <stevenharradine@gmail.com>
2021-04-10 18:22:22 -0400

    convert innerhtml to proper js dom creation and manipulation

--- docroot/views/productDetails.php
+++ docroot/views/productDetails.php
@@ -2,6 +2,9 @@
 	h2 {
 		display: inline-block;
 	}
+  h2, #productName input[type="text"] {
+    margin: 15px 5px;
+  }
   .left {
     float: left;
     text-align: center;
@@ -19,7 +22,7 @@
 <?php
     while($row = $result->fetch_assoc()) {
 ?>
-      <div><h2><?php echo $row[name]; ?></h2><input onclick='editName()' type='button' value='Edit' /></div>
+      <div id="productName"><h2><?php echo $row[name]; ?></h2><span onclick='editName()' class='button'>Edit</span></div>
       <div class="left">
         <img class='product' src='images/products/<?php echo $row[barcode]; ?>.jpg' />
         <svg class='barcode' jsbarcode-value='<?php echo $row[barcode]; ?>' jsbarcode-textmargin='0' jsbarcode-fontoptions='bold'></svg>
@@ -36,19 +39,58 @@
       JsBarcode(".barcode").init();
 
       function editName () {
-		currentName = document.getElementsByTagName("h2")[0].innerHTML
-		document.getElementsByTagName("h2")[0].parentElement.innerHTML = "<input name='name' type='text' value='" + currentName + "' placeholder='" + currentName + "' /><input onclick='closeEditName()' type='button' value='Cancel' /><input onclick='saveName()' type='button' value='Save' />"
+    		currentName = document.getElementsByTagName("h2")[0].innerHTML
 
+        clearProductName()
+
+        inputElement = document.createElement ("input")
+        inputElement.setAttribute ("name", "name")
+        inputElement.setAttribute ("type", "text")
+        inputElement.setAttribute ("placeholder", currentName)
+        inputElement.setAttribute ("value", currentName)
+
+        cancelElement = document.createElement("span")
+        cancelElement.classList.add ("button")
+        cancelElement.onclick = function () { closeEditName() }
+        cancelElement.textContent = "Cancel"
+
+        saveElement = document.createElement("span")
+        saveElement.classList.add ("button")
+        saveElement.onclick = function () { saveName() }
+        saveElement.textContent = "Save"
+
+        document.getElementById("productName").appendChild(inputElement)
+        document.getElementById("productName").appendChild(cancelElement)
+        document.getElementById("productName").appendChild(saveElement)
+      }
+
+      function clearProductName () {
+        parent = document.getElementById("productName")
+        while (parent.firstChild) {
+            parent.firstChild.remove()
+        }
       }
 
       function closeEditName () {
       	currentName = document.getElementsByName("name")[0].getAttribute("placeholder")
-      	document.getElementsByName("name")[0].parentElement.innerHTML = "<h2>" + currentName + "</h2><input onclick='editName()' type='button' value='Edit' />"
+
+        clearProductName()
+
+        h2Element = document.createElement("h2")
+        h2Element.textContent = currentName
+
+        editElement = document.createElement("span")
+        editElement.classList.add ("button")
+        editElement.onclick = function () { editName() }
+        editElement.textContent = "Edit"
+
+        document.getElementById("productName").appendChild(h2Element)
+        document.getElementById("productName").appendChild(editElement)
       }
 
       function saveName () {
       	currentName = document.getElementsByName("name")[0].value
       	barcode = document.getElementsByTagName("text")[0].innerHTML
-		location.href=location.pathname+"?barcode="+barcode+"&action=updateName&name="+currentName
+		    location.href=location.pathname+"?barcode="+barcode+"&action=updateName&name="+currentName
       }
     </script>
