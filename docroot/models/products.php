1b085244262e8a16483538a52acfca78df9292cb
Steven Harradine <stevenharradine@gmail.com>
2021-04-11 21:24:39 -0400

    use prepared statements

--- docroot/models/products.php
+++ docroot/models/products.php
@@ -3,18 +3,26 @@
 
 	class Products {
 		function incQty ($dbConnection, $barcode) {
-			$sql = "UPDATE products SET qty = qty + 1 where barcode=$barcode";
-			$result = $dbConnection->query($sql);
+			$stmt = $dbConnection->prepare("UPDATE products SET qty = qty + 1 where barcode=?");
+			$stmt->bind_param("i", $barcode);
+			$stmt->execute();
+			$stmt->close();
 		}
 
 		function decQty ($dbConnection, $barcode) {
-			$sql = "UPDATE products SET qty = qty - 1 where barcode=$_POST[barcode]";
-			$result = $dbConnection->query($sql);
+			$stmt = $dbConnection->prepare("UPDATE products SET qty = qty - 1 where barcode=?");
+			$stmt->bind_param("i", $barcode);
+			$stmt->execute();
+			$stmt->close();
 		}
 
 		function search_products_by_barcode ($dbConnection, $barcode) {
 			$sql = "SELECT * from `products` where barcode=$barcode";
-			$result = $dbConnection->query($sql);
+			$stmt = $dbConnection->prepare($sql);
+			$stmt->bind_param("i", $barcode);
+			$stmt->execute();
+			$result = $stmt->get_result();
+			$stmt->close();
 
 			return $result;
 		}
